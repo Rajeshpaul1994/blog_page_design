bd3dcbc5a736d071ed78d2456916a6d650f4b546
Rajeshpaul1994 <rajeshkumarpaul94@gmail.com>
2023-04-10 23:20:29 +0530

    add react and ts vite

---
+++ blog_template_react/src/App.tsx
@@ -0,0 +1,12 @@
+import Logo from './assets/img/plants-care-logo.svg'
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
